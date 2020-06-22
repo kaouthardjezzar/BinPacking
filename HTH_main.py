e684c7ae96dbd4c311afec066df82ffc737116e4
Hichem Bouslama <gh_bouslama@esi.dz>
2020-06-22 01:05:32 +0100

    Modified HTH after parameter calibration

--- HTH_main.py
+++ HTH_main.py
@@ -1,10 +1,9 @@
-from SimulatedAnnealing import SA
-from HRH_AG_RS import HRH_RS
+import math
 from item import Item
 from random import shuffle
 from datetime import datetime
 import json
-from genetic_algorithm import GeneticAlgorithm
+from genetic_algorithm_elite import GeneticAlgorithm
 from HTH import *
 
 def log(message, end=None):
@@ -13,7 +12,23 @@ def log(message, end=None):
 
 if __name__ == '__main__':
     datasets = [
-       {"name": "N1C1W4_E.txt", "results": {}},
+        # {"name": "HARD9.txt", "solution":56, "results": {}},
+        # {"name": "HARD0.txt", "solution":56, "results": {}},
+        # {"name": "HARD6.txt", "solution":57, "results": {}},
+        # {"name": "HARD7.txt", "solution":55, "results": {}},
+
+        {"name": "N1C1W4_E.txt", "solution":38, "results": {}},
+        # {"name": "N1C2W2_H.txt", "solution":23, "results": {}},
+        # {"name": "N1C3W1_C.txt", "solution":17, "results": {}},
+        # {"name": "N2C1W1_G.txt", "solution":60, "results": {}},
+        # {"name": "N2C2W1_F.txt", "solution":49, "results": {}},
+        # {"name": "N2C3W1_Q.txt", "solution":34, "results": {}},
+        # {"name": "N3C1W1_K.txt", "solution":102, "results": {}},
+
+        #{"name": "N1W4B2R3.txt", "solution":6, "results": {}},
+        # {"name": "N2W1B3R8.txt", "solution":34, "results": {}}, 
+        # {"name": "N3W3B3R1.txt", "solution":27, "results": {}}, 
+        #{"name": "N4W4B3R9.txt", "solution":56, "results": {}}, 
 
         
       
@@ -28,20 +43,22 @@ if __name__ == '__main__':
             log("\n\nDATASET {}: num_items {} capacity {} items_read {}".format(dataset["name"], num_items, capacity, len(items)))
         items = [Item(size=int(i)) for i in items]
         
-        log("  Iteration", end=" ")
+        nbthread = 6
         # Perform 30 independent iterations.
-        for iteration in range(2):
-            log(iteration+1, end=" ")
-            # Randomize the order of the items in the item list.
-            shuffle(items)
-            thing = HTH(capacity, items)
-            start_time = datetime.now()
-            sa = thing.run()
-            execution_time = datetime.now() - start_time
-            # Record the relevant data for analysis
+        
+        # Randomize the order of the items in the item list.
+        shuffle(items)
+        thing = HTH(capacity, items,nbthread,300,60,60,20,0.1,0.7)
+        print("Recherche des solutions en cours ,nombre de threads actifs: " + str(nbthread))
+        start_time = datetime.now()
+        solutions = thing.run()
+        execution_time = datetime.now() - start_time
+        # Record the relevant data for analysis
+        for s in solutions:
             summary = {
-                "execution_time": str(execution_time),
-                "num_bins": [len(a) for a in sa],
+                "execution_time": str(s[1]),
+                "num_bins": s[0].num_bins,
+                "combination": s[0].pattern,
             }
             dataset["results"].setdefault("HTH", []).append(summary)
             #dataset["results"].setdefault("SA", []).append(summary)
